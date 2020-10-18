14a7c1ddbd1f93135c05ad785f2f23f14788c6b3
Malte Dömer <maltedoemer@gmail.com>
2020-10-18 18:06:10 +0200

    bootsector working with lba

--- bios/boot.asm
+++ bios/boot.asm
@@ -1,17 +1,20 @@
 bits 16
-cpu 8086
 section .boot
 
 global start
 
 extern entry
-extern stage2_sectors
-
+extern sect_cnt
 
 start:
     jmp short skipfat
     nop
     times 90 -($-$$) db 0
+
+    ; the LBA of second stage
+    ; will be changed by installer
+lba_low dd 0
+lba_high dd 0
 
 skipfat:
     ; disable interrupts and clear direction flag
@@ -45,7 +48,62 @@ next:
     ; save boot drive
     mov byte [drive], dl
 
+    ; used by lba_mode and chs_mode
+    mov si, disk_info
+
+    ; check for LBA extensions
+    mov ah, 0x41
+    mov bx, 0x55AA
+    int 0x13
+
+    jc chs_mode
+    cmp bx, 0xAA55
+    jne chs_mode
+    and cx, 1
+    jz chs_mode
+
+lba_mode:
+    ; configure the DAP pointed to by si    
+
+    ; set size and zero byte
+    mov word [si], 0x1000
+
+    ; set sector count
+    mov word [si + 2], sect_cnt
+
+    ; copy low 32-bits of LBA
+    mov eax, dword [lba_low]
+    mov dword [si + 8], eax
+
+    ; copy high 32-bits of LBA
+    mov eax, dword [lba_high]
+    mov dword [si + 12], eax
+
+    ; set segment to zero and offset to 0x800
+    mov eax, 0x800
+    mov dword [si + 4], eax
+
+    ; get the boot drive
+    mov dl, byte [drive]
+
+    ; int 0x13 ah = 0x42 to read sectors from disk into memory
+    mov ah, 0x42
+    int 0x13
+
+    ; on error fall back to chs
+    jc chs_mode
+
+    ; go on to the next stage
+    jmp entry
+
+chs_mode:
+
+
+
+
 
 
 section .bss
-    drive resb 1
+mode resb 1
+disk_info resb 16
+drive resb 1
